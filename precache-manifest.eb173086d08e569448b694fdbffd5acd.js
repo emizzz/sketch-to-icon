ba6bfb93f5463531bc8e994566dead268c6b65e8
emizzz <emiliuzz@outlook.it>
2020-04-11 13:31:06 +0200

    Updates

---
+++ precache-manifest.eb173086d08e569448b694fdbffd5acd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b85041b9dd0bc54d648dfee9f0e0bf42",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dd7b3fb77ad0c89eb0cc",
+    "url": "/static/css/main.d7a24d92.chunk.css"
+  },
+  {
+    "revision": "47fb898b78613ebb5d07",
+    "url": "/static/js/2.e5292099.chunk.js"
+  },
+  {
+    "revision": "665fca30b0b108dbef9e861225934720",
+    "url": "/static/js/2.e5292099.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd7b3fb77ad0c89eb0cc",
+    "url": "/static/js/main.4dde8806.chunk.js"
+  },
+  {
+    "revision": "84c98e80d36831bed132",
+    "url": "/static/js/runtime-main.47f8ed00.js"
+  }
+]);
